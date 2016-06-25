8059f14c0cc4fe78e2da4772cf0d8718e0be9d0f
sonerik <thesonerik@gmail.com>
2016-06-26 02:48:28 +0300

    Initial commit.

---
+++ src/main/groovy/com/github/s0nerik/rxandroid/groovy/RxAndroidExtensions.groovy
@@ -0,0 +1,24 @@
+package com.github.s0nerik.rxandroid.groovy
+
+import groovy.transform.CompileStatic
+import rx.Observable
+import rx.Scheduler
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+@CompileStatic
+class RxAndroidExtensions {
+    static <T> Observable<T> applySchedulers(final Observable<T> observable) {
+        observable.compose({
+                    it.subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+        } as Observable.Transformer)
+    }
+
+    static <T> Observable<T> applySchedulers(final Observable<T> observable, Scheduler subscribeOn, Scheduler observeOn) {
+        observable.compose({
+            it.subscribeOn(subscribeOn)
+                    .observeOn(observeOn)
+        } as Observable.Transformer)
+    }
+}
